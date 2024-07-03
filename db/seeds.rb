de92e0f088fd7140663413e79c8343666c7b6e18
Sofeanna17 <1007016@mymail.sutd.edu.sg>
2024-07-03 17:57:18 +0800

    feature: edit title

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Clear existing videos
+Video.destroy_all
+
+# Create new videos
 Video.create([
   { title: 'Sample Video 1', date: Date.parse('2024-07-01'), path: '/videos/sample1.mp4' },
-  { title: 'Sample Video 2', date: Date.parse('2024-07-02'), path: '/videos/sample2.mp4' }
+  { title: 'Sample Video 2', date: Date.parse('2024-07-02'), path: '/videos/sample2.mp4' },
+  { title: 'Sample Video 3', date: Date.parse('2024-07-03'), path: '/videos/sample3.mp4' }
 ])
