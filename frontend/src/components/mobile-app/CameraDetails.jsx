fdc5c5d978c658fb8967532c820dcceb5d648cb7
jingkai27 <jingkai.t27@gmail.com>
2024-07-15 10:23:01 +0800

    hehe

--- frontend/src/components/mobile-app/CameraDetails.jsx
+++ frontend/src/components/mobile-app/CameraDetails.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
 const CameraDetail = () => {
-    const { id } = useParams();
-    const location = useLocation();
-    const { cameraData } = location.state || {};
+  const { id } = useParams();
+  const location = useLocation();
+  const { cameraData } = location.state || {};
 
-    return (
+  return (
+    <div>
+      <h1>Camera Details</h1>
+      {cameraData && (
         <div>
-            <h1>Camera Details</h1>
-            {cameraData && (
-                <div>
-                    <p>ID: {cameraData.id}</p>
-                    <p>Name: {cameraData.camera_name}</p>
-                    <p>Status: {cameraData.status}</p>
-                    <p>App ID: {cameraData.app_id}</p>
-                    <p>Channel: {cameraData.channel}</p>
-                    <p>Token: {cameraData.token}</p>
-                    <p>Created At: {cameraData.created_at}</p>
-                    <p>Updated At: {cameraData.updated_at}</p>
-                </div>
-            )}
+          <p>ID: {cameraData.id}</p>
+          <p>Name: {cameraData.camera_name}</p>
+          <p>Status: {cameraData.status}</p>
+          <p>App ID: {cameraData.app_id}</p>
+          <p>Channel: {cameraData.channel}</p>
+          <p>Token: {cameraData.token}</p>
+          <p>Created At: {cameraData.created_at}</p>
+          <p>Updated At: {cameraData.updated_at}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default CameraDetail;
