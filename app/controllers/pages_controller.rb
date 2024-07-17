28922c0e919e6bdf5c436de1f2b754cf0d08b761
jyayin <jyayinn@gmail.com>
2024-07-17 23:12:48 +0800

    pulling vids from bucket works properly now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,14 @@ class PagesController < ApplicationController
   def camera
     @user_setting = current_user.user_setting || current_user.create_user_setting
   end
+
+  def camera_broadcast
+    @user_setting = current_user.user_setting
+
+    unless @user_setting
+      flash[:error] = "User settings not found"
+      redirect_to some_path and return
+    end
+  end
 end
   
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,16 @@
 class Video < ApplicationRecord
     belongs_to :user
     has_one_attached :file
+
+    after_commit :set_duration, on: :create
+
+    private
+
+    def set_duration
+        return unless file.attached?
+
+        video_path = ActiveStorage::Blob.service.send(:path_for, file.key)
+        movie = FFMPEG::Movie.new(video_path)
+        update(duration: movie.duration)
+    end
 end
