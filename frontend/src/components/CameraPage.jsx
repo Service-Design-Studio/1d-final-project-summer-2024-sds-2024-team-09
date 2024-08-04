bd21bda5d858a1d9d6196d5abfd1b60b8c6e825b
jingkai27 <jingkai.t27@gmail.com>
2024-08-04 21:31:19 +0800

    lala

--- frontend/src/components/CameraPage.jsx
+++ frontend/src/components/CameraPage.jsx
@@ -42,7 +42,7 @@ const CameraPage = () => {
     }, []);
 
     const APP_ID = "d543c7c876734c4a82da72a588edd047";
-    const TOKEN = '007eJxTYDgn6XW/bEH6yZVbzil8W7L/iUB2YuWmNuPr99atfhz4zOGFAkOKqYlxsnmyhbmZubFJskmihVFKorlRoqmFRWpKioGJuezkNWkNgYwMv0wLWBgZIBDEZ2NISqzMTaxgYAAAkagjYg==';
+    const TOKEN = '007eJxTYHjwOHWz89LXQVGTdjGs4szvP+PC+u6Z8RueqUya57erS0UqMKSYmhgnmydbmJuZG5skmyRaGKUkmhslmlpYpKakGJiYv2xYn9YQyMjQWPmahZEBAkF8NoakxMrcxAoGBgCguiDB';
     const CHANNEL = "baymax";
 
     // const APP_ID = cameraData.app_id;
